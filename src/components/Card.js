ac6bd9e9c570d54abd47d222e88f67af76d489d3
v3r <v3rslor@protonmail.com>
2022-06-01 16:01:13 +0200

    delete card now dont reload page

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,17 @@ import React, { Component, useState } from 'react';
 
 
 
-class Card extends Component {
-	constructor () {
-		super();
-		this.state = { color: {color: 'rgb(196, 196, 196)'}};
-	}	
+const Card = ({movie, funcOfParent, parentListData}) => {
 
-	formatDate(date) {
+
+	const [colorIcons, setcolorIcons] = useState({color: 'rgb(196, 196, 196)'});
+
+	function formatDate(date) {
 		const [yy, mm, dd] = date.split('-');
 		return [dd, mm, yy].join('/');
 	};
 	
-	findGenre(genres, token) {
+	function findGenre(genres, token) {
 		const new_list = [];
 		const genreDict = { '28': "Action ", '12': "Adventure ", '16': "Animation ", '35': "Comedy ", '80': "Crime ", '99': "Documentary ", '18': "Drama ", '10751': "Family ", '14': "Fantasy ", '36': "History ", '27': "Horror ", '10402': "Music ", '9648': "Mystery ", '10749': "Romance ", '878': "Science Fiction ", '10770': "TV Movie ", '53': "Thriller ", '10752': "War ", '37': "Western "};
 		if (token === 1) {
@@ -28,36 +27,44 @@ class Card extends Component {
 		return (new_list.map((genre, i) => <li key={i}>{genre}</li>))
 	};
 	
-	handleStorageData(id, token) {
+	function handleStorageData(id, token) {
 		let data_list = window.localStorage.movie ? window.localStorage.movie.split(',') : [];
+
 		if (!data_list.includes(id.toString()) && token === 1) { // add data to localStorage
 			data_list.push(id);
 			window.localStorage.movie = data_list;
-		} else if (token === 2) { // delete data of localStorage
-			let newDataList = data_list.filter((idm) => idm != id);
+		} 
+		else if (token === 2) { // delete data of localStorage
+			let newDataList = data_list.filter((eachId) => eachId != id);
 			window.localStorage.movie = newDataList;
-			window.location.reload();
+			const newArr = parentListData.filter(item => newDataList.includes(item.id.toString()));
+			funcOfParent(newArr);
 		}
+	};
+	
+	function setupColorIcons() {
+		const storageList = window.localStorage.movie ? window.localStorage.movie.split(',') : [];
+		storageList.map((id) => id.toString() == movie.id ? setcolorIcons({color: 'red'}) : {color: 'red'})
 	}
-	render() { 
-		const movie = this.props.movie;
-		return (
-			<div className="card">
-				{/* <img src={movie.poster_path ? "https://image.tmdb.org/t/p/w500/" + movie.poster_path : "./img/poster.jpg"} alt="movie-poster" /> */}
-				<h2>{movie.title}</h2>
-				{movie.release_date ? <h5>Release date : {this.formatDate(movie.release_date)}</h5> : <h5>date unavailable</h5>}
-				<h4>{movie.vote_average}/10 <span>⭐</span></h4>
-				{movie.genre_ids? <ul>{this.findGenre(movie.genre_ids, 1)}</ul> : movie.genres ? <ul>{this.findGenre(movie.genres, 0)}</ul> : ""}
-				{movie.overview ? <h3>Synopsis</h3> : ""}
-				<p>{movie.overview}</p>
-				{movie.genre_ids ? <div className="btn" 
-				onClick={() => {
-					this.handleStorageData(movie.id, 1); 
-					this.setState({color: {color: 'red'}})}
-					}><i style={this.state.color} className="fas fa-solid fa-heart"></i></div> : <div className="btn" onClick={() => this.handleStorageData(movie.id, 2)}><i className="fas fa-solid fa-trash"></i>	</div>}
-			</div>
-		);
-	}
-}
+
+	return (
+		<div className="card">
+			{/* {setupColorIcons()} */}
+			<img src={movie.poster_path ? "https://image.tmdb.org/t/p/w500/" + movie.poster_path : "./img/poster.jpg"} alt="movie-poster" />
+			<h2>{movie.title}</h2>
+			{movie.release_date ? <h5>Release date : {formatDate(movie.release_date)}</h5> : <h5>date unavailable</h5>}
+			<h4>{movie.vote_average}/10 <span>⭐</span></h4>
+			{movie.genre_ids? <ul>{findGenre(movie.genre_ids, 1)}</ul> : movie.genres ? <ul>{findGenre(movie.genres, 0)}</ul> : ""}
+			{movie.overview ? <h3>Synopsis</h3> : ""}
+			<p>{movie.overview}</p>
+			{movie.genre_ids ? <div className="btn" 
+			onClick={() => {
+				handleStorageData(movie.id, 1);
+				setcolorIcons({color: 'red'})
+				}}><i style={colorIcons} className="fas fa-solid fa-heart"></i></div> : <div className="btn" onClick={() => handleStorageData(movie.id, 2)}><i className="fas fa-solid fa-trash"></i>	</div>}
+		</div>
+	);
+};
+
  
 export default Card;
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -6,10 +6,9 @@ import Card from '../components/Card'
 const UserList = () => {
 
 	const [listData, setlistData] = useState([]);
-
+	const [count, setCount] = useState(0);
 	useEffect(() => {
-		const moviesList = window.localStorage.movie ? window.localStorage.movie.split(',') : []; 
-
+		const moviesList = window.localStorage.movie ? window.localStorage.movie.split(',') : [];
 		for (let i = 0; i < moviesList.length; i++)
 		{			
 			axios.get(`https://api.themoviedb.org/3/movie/${moviesList[i]}external_ids?api_key=f2325b7f071669430ffa93fbe85df4ad`)
@@ -17,12 +16,18 @@ const UserList = () => {
 		}
 	}, []);
 
+	function changeStateTest(data)
+	{
+		setlistData(data);
+	}
+
+
 	return (
 			<div className="user-list-page">
 				<Header/>
-				<h2><span>❤️<i className='fas fa-github'></i></span> My Favlist</h2>
+				<h2><span>❤️</span> My Favlist</h2>
 				<div className="result">
-					{listData.map((movie) => <Card key={movie.id} movie={movie}/>)}
+					{listData.map((movie) => <Card key={movie.id} movie={movie} funcOfParent={changeStateTest} parentListData={listData}/>)}
 				</div>
 			</div>
 	);
